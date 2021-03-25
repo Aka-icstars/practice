95fd8634a7432eef78b2bb87b917c3213cf57019
Aka-icstars <80833493+Aka-icstars@users.noreply.github.com>
2021-03-24 22:29:37 -0500

    Add files via upload

---
+++ practice/game.js
@@ -0,0 +1,17 @@
+function startgame() {
+    myGameArea.start();
+}
+
+{
+    // declare name 
+    let name ="tom";
+    console.log(name);
+}
+
+
+{
+    // declare name in another block
+    let name ="jerry";
+    console.log(name);
+}
+
